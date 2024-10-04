31a3637fbe57feff23e85f2cbb76a219bf9b016e
jeremy1214 <s1110280@my.cmsh.cyc.edu.tw>
2024-10-05 02:06:16 +0800

    [add] react

---
+++ index.tsx
@@ -0,0 +1,41 @@
+import type { ComponentType } from "react"
+
+// Learn more: https://www.framer.com/docs/guides/overrides/
+
+
+
+export function withRotate(Component): ComponentType {
+    return (props) => {
+        return (
+            <Component
+                {...props}
+                animate={{ rotate: 90 }}
+                transition={{ duration: 2 }}
+            />
+        )
+    }
+}
+
+export function withHover(Component): ComponentType {
+    return (props) => {
+        return <Component {...props} whileHover={{ scale: 1.05 }} />
+    }
+}
+
+export function withRandomColor(Component): ComponentType {
+    return (props) => {
+        const [store, setStore] = useStore()
+
+        return (
+            <Component
+                {...props}
+                animate={{
+                    background: store.background,
+                }}
+                onClick={() => {
+                    setStore({ background: randomColor() })
+                }}
+            />
+        )
+    }
+}
